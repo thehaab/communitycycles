7b247635051bbd2c93e1b2a818a7bbae9d169fe7
Travis Church <thehaab@gmail.com>
2024-01-14 12:42:51 -0600

    update

--- script.js
+++ script.js
@@ -12,52 +12,9 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-const bikeImage = document.getElementById('bikeImage');
-const overlayLinks = document.getElementById('overlayLinks');
-const overlay = document.getElementById('overlay');
-
-bikeImage.addEventListener('mouseover', showLinks);
-bikeImage.addEventListener('mouseout', hideLinks);
-
-function showLinks(event) {
-  const x = event.clientX - bikeImage.offsetLeft;
-  const y = event.clientY - bikeImage.offsetTop;
-
-  // Update links based on mouse position
-  overlayLinks.style.left = x + 'px';
-  overlayLinks.style.top = y + 'px';
-}
-
-function hideLinks() {
-  // Hide links on mouseout
-  overlayLinks.style.left = '-9999px';
-  overlayLinks.style.top = '-9999px';
-}
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const buttons = document.querySelectorAll('#language-buttons button');
-  const contentEn = document.getElementById('content-en');
-  const contentEs = document.getElementById('content-es');
-
-  buttons.forEach((button) => {
-    button.addEventListener('click', function () {
-      const language = this.getAttribute('data-language');
-      if (language === 'en') {
-        contentEn.style.display = 'block';
-        contentEs.style.display = 'none';
-      } else if (language === 'es') {
-        contentEn.style.display = 'none';
-        contentEs.style.display = 'block';
-      }
-    });
-  });
-});
 
 const quotes = [
   "Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein",
-  
-  "Bicycling is a big part of the future. There's something wrong with a society that drives a car to work out in a gym. - Bill Nye",    "Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein",
 
   "Bicycling is a big part of the future. It has to be. There's something wrong with a society that drives a car to work out in a gym. - Bill Nye",
 
@@ -90,5 +47,5 @@ function displayNextQuote() {
 // Display the first quote
 displayNextQuote();
 
-// Change quote every 5 seconds (adjust as needed)
-setInterval(displayNextQuote, 5000);
+// Change quote every 10 seconds (adjust as needed)
+setInterval(displayNextQuote, 10000);
