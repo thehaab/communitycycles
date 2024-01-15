68008686deadaf2f7fc15d4033f4edf5b2087b20
Travis Church <thehaab@gmail.com>
2024-01-15 11:50:46 -0600

    update

--- script.js
+++ script.js
@@ -14,25 +14,25 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 const quotes = [
-  "Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein",
+  '<em>"Life is like riding a bicycle. To keep your balance, you must keep moving."</em><br> - Albert Einstein',
 
-  "Bicycling is a big part of the future. It has to be. There's something wrong with a society that drives a car to work out in a gym. - Bill Nye",
+  `<em>"Bicycling is a big part of the future. It has to be. There's something wrong with a society that drives a car to work out in a gym."</em><br> - Bill Nye`,
 
-  "When man invented the bicycle, he reached the peak of his attainments. - Elizabeth West",
+  `<em>"When man invented the bicycle, he reached the peak of his attainments."</em><br> - Elizabeth West`,
 
-  "A bicycle ride around the world begins with a single pedal stroke. - Scott Stoll",
+  `<em>"A bicycle ride around the world begins with a single pedal stroke."</em><br> - Scott Stoll`,
 
-  "It is by riding a bicycle that you learn the contours of a country best since you have to sweat up the hills and coast down them. - Ernest Hemingway",
+  `<em>"It is by riding a bicycle that you learn the contours of a country best since you have to sweat up the hills and coast down them."</em><br> - Ernest Hemingway`,
 
-  "Bicycles may change, but cycling is timeless. - Zapata Espinoza",
+  `<em>"Bicycles may change, but cycling is timeless."</em><br> - Zapata Espinoza`,
 
-  "The bicycle is a curious vehicle. Its passenger is its engine. - John Howard",
+  `<em>"The bicycle is a curious vehicle. Its passenger is its engine."</em><br> - John Howard`,
 
-  "Every time I see an adult on a bicycle, I no longer despair for the future of the human race. - H.G. Wells",
+  `<em>"Every time I see an adult on a bicycle, I no longer despair for the future of the human race."</em><br> - H.G. Wells`,
 
-  "Melancholy is incompatible with bicycling. - James E. Starrs",
+  `<em>"Melancholy is incompatible with bicycling."</em><br>- James E. Starrs`,
 
-  "A bicycle does get you there and more... And there is always the thin edge of danger to keep you alert and comfortably apprehensive. - Bill Emerson",
+  `<em>"A bicycle does get you there and more... And there is always the thin edge of danger to keep you alert and comfortably apprehensive."</em><br> - Bill Emerson`,
   // Add more quotes as needed
 ];
 
