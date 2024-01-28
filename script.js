d2824243676127ad6635acb4af9c4c8baec17abb
Travis Church <thehaab@gmail.com>
2024-01-28 17:10:52 -0600

    update

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const observer = new IntersectionObserver((entries) => {
   })
 })
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+/* const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); */
 
 
 const quotes = [
